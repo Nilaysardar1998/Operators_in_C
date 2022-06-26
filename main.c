d4efd5a85d6ffa5d2e40a3ed5548c34c7e01948b
NILAY SARDAR <nilay.sardar04121998@gmail.com>
2022-06-26 10:34:45 +0530

    updated

--- main.c
+++ main.c
@@ -2,8 +2,10 @@
 #include <stdio.h>
 
 int main() {
-    //Arithmetic operators in C
+
     int a = 2,b = 4, result;
+
+    //Arithmetic operators in C
     //(+) Arithmetic operator
     result = a + b;
     printf("%d + %d = %d\n", a, b, result);
@@ -26,7 +28,7 @@ int main() {
     result = a-- + b--;
     printf("%d - %d = %d\n", a--, b--, result);
 
-    //Assignment Operator
+    //Assignment Operator in C
     //(=) Assignment operator
     int x = 10;
     printf("Value of x using (=) assignment operator is %d\n", x);
@@ -45,5 +47,14 @@ int main() {
     x %= 4;
     //(%=) Assignment operator
     printf("Value of x using (%=) assignment operator is %d\n", x);
+
+    //Comparing Operators in C
+    //(==) Comparing operator
+    int y = 10; int z = 20;
+    printf("y == z: (%d) == (%d) always false (%d)\n", y, z, y == z);
+    z = 10;
+    printf("y == z: (%d) == (%d) always true (%d)\n", y, z, y == z);
+
+
     return 0;
 }
